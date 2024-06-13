ceb3cc279277e2c4ee2c384af941bec5a993ea20
adilaguenagay <drmanager67@gmail.com>
2024-06-13 15:36:52 +0100

    V1 TP ANG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommandeViewComponent } from './view/commande/commande-view/commande-view.component';
+import { CommandeEditComponent } from './view/commande/commande-edit/commande-edit.component';
+import { CommandeListComponent } from './view/commande/commande-list/commande-list.component';
+import { CommandeCreateComponent } from './view/commande/commande-create/commande-create.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommandeViewComponent,
+    CommandeEditComponent
   ],
   imports: [
+    HttpClientModule,
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommandeListComponent,
+    CommandeCreateComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
